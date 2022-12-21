f97e8b0528b6a96f6c72156d3aa406dba7addd7a
Pierre Chambart <pierre.chambart@ocamlpro.com>
2022-12-21 11:35:53 +0100

    Update for binarien array.init_static

--- wasm/wast.ml
+++ wasm/wast.ml
@@ -83,7 +83,7 @@ module C = struct
 
   let array_new_canon_fixed typ size args =
     match mode with
-    | Binarien -> node "array.new" ([ type_name typ ] @ args)
+    | Binarien -> node "array.init_static" ([ type_name typ ] @ args)
     | Reference ->
       node "array.new_canon_fixed"
         ([ type_name typ; Int (Int64.of_int size) ] @ args)
